ea792d67c6e7fd392e07d9fddce0879894fbe295
sumit.ml@gmail.com <sumit.ml@gmail.com>
2018-10-07 01:45:21 +0530

    added owners list

--- sumit-pet-clinic-web/src/main/java/com/sk/sumitpetclinic/controllers/OwnerController.java
+++ sumit-pet-clinic-web/src/main/java/com/sk/sumitpetclinic/controllers/OwnerController.java
@@ -1,13 +1,26 @@
 package com.sk.sumitpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sk.sumitpetclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
 	
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+
+
 	@RequestMapping({"/owners","/owners/index","/owners/index.html"})
-	public String listOwners() {
+	public String listOwners(Model model) {
+		model.addAttribute("owners",ownerService.findAll());
 		return "owners/index";
 	}
 
